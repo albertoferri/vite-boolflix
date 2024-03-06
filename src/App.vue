e4a7127765bc3641a3b110928278b21f8ca721ed
albertoferri <falberto99@gmail.com>
2024-03-06 11:38:58 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+
+import AppSearch from './components/AppSearch.vue';
+
+export default{
+
+  components: {
+    AppSearch,
+  }
+
+
+
+}
+
+
+
+</script>
+
+<template>
+
+<div class="container">
+  <AppSearch></AppSearch>
+
+</div>
+ 
+</template>
+
+<style lang="scss">
+
+</style>
